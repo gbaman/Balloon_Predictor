f98c2ae1ad3c310c4e0bfc48c67598bb190484bc
Andrew Mulholland <gbaman@users.noreply.github.com>
2021-10-24 00:12:41 +0100

    Add hourly mode

--- balloon_predictor/main.py
+++ balloon_predictor/main.py
@@ -1,12 +1,32 @@
 from flask import Flask, render_template
 import map_builder
+from config import LOCATIONS, DEFAULT_LOCATION
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    map_data = map_builder.generate_flights()._repr_html_()
+    map_data = map_builder.generate_launch_flights()._repr_html_()
     return render_template("map.html", map_data=map_data, raw_flights=map_builder.raw_flights)
+
+
+@app.route("/hourly")
+def hourly():
+    map_data = map_builder.generate_hourly_flights(DEFAULT_LOCATION)._repr_html_()
+    return render_template("map.html", map_data=map_data, raw_flights=map_builder.raw_flights)
+
+
+@app.route("/hourly/<location_name>")
+def hourly_location(location_name):
+    found_location = None
+    locations_str = ""
+    for location in LOCATIONS:
+        location_lower = location[0].lower().replace(" ", "_")
+        locations_str = f"{locations_str}<br>- {location_lower}"
+        if location_lower == location_name:
+            map_data = map_builder.generate_hourly_flights(location)._repr_html_()
+            return render_template("hourly_map.html", map_data=map_data, raw_flights=map_builder.raw_flights)
+    return f"Error - Location {found_location} does not exist. Available locations can be found below. {locations_str}"
 
 if __name__ == '__main__':
     app.run()
--- balloon_predictor/map_builder.py
+++ balloon_predictor/map_builder.py
@@ -6,15 +6,15 @@ from typing import List
 import folium
 import requests
 
+from config import CASTOR_BAY, FURNEUX, VINCENT_SQUARE, COLEMORE
+
 PREDICTOR_URL = "http://predict.cusf.co.uk/api/v1/"
-VINCENT_SQUARE = ["Vincent Square", "51.4934", "-0.1351"]
-FURNEUX = ["Furneux", "51.9314", "0.0795"]
-COLEMORE = ["Colemore Common", "51.0622", "-1.0097"]
 #TITLE = "Launches from Vincent Square, Furneux and Colemore Common on the 4th and 5th July 2021"
 TITLE = "Launches from Vincent Square, Furneux and Colemore Common on the 8th July 2021"
+HOURLY_TITLE = "Hourly launches"
 
 DATE1 = "2021-07-05"
-DATE2 = "2021-07-08"
+DATE2 = "2021-10-30"
 
 
 class Flight():
@@ -45,20 +45,20 @@ raw_flights = [
     #Flight(COLEMORE[0], COLEMORE[1], 20000, 4, 5, f"{DATE1}T10:30:00Z", "Colemore", "yellow", "purple", 350),
     #Flight(COLEMORE[0], COLEMORE[1], 30000, 5, 5, f"{DATE1}T10:30:00Z", "Colemore", "yellow", "purple", 1000),
 
+    Flight(VINCENT_SQUARE, 26000, 5, 5, f"{DATE2}T10:30:00Z", "Vincent Square", "pink", "orange", 600),
+    Flight(VINCENT_SQUARE, 26000, 4, 5, f"{DATE2}T10:30:00Z", "Vincent Square", "green", "orange", 600),
+    Flight(VINCENT_SQUARE, 26000, 3, 5, f"{DATE2}T10:30:00Z", "Vincent Square", "purple", "orange", 350),
+    Flight(VINCENT_SQUARE, 26000, 2, 5, f"{DATE2}T10:30:00Z", "Vincent Square", "orange","orange", 200),
 
-    Flight(VINCENT_SQUARE, 27000, 5, 5, f"{DATE2}T03:30:00Z", "Vincent Square", "green", "orange", 600),
-    Flight(VINCENT_SQUARE, 20000, 4, 5, f"{DATE2}T03:30:00Z", "Vincent Square", "purple", "orange", 350),
-    Flight(VINCENT_SQUARE, 12700, 3, 5, f"{DATE2}T03:30:00Z", "Vincent Square", "orange","orange", 200),
+    #Flight(FURNEUX, 27000, 5, 5, f"{DATE2}T10:30:00Z", "Furneux", "green", "purple", 600),
+    #Flight(FURNEUX, 20000, 4, 5, f"{DATE2}T10:30:00Z", "Furneux", "purple", "purple", 350),
+    #Flight(FURNEUX, 12700, 3, 5, f"{DATE2}T10:30:00Z", "Furneux", "orange", "purple", 200),
+    #Flight(FURNEUX, 30000, 5, 5, f"{DATE2}T10:30:00Z", "Furneux", "red", "purple", 1000),
 
-    Flight(FURNEUX, 27000, 5, 5, f"{DATE2}T10:30:00Z", "Furneux", "green", "purple", 600),
-    Flight(FURNEUX, 20000, 4, 5, f"{DATE2}T10:30:00Z", "Furneux", "purple", "purple", 350),
-    Flight(FURNEUX, 12700, 3, 5, f"{DATE2}T10:30:00Z", "Furneux", "orange", "purple", 200),
-    Flight(FURNEUX, 30000, 5, 5, f"{DATE2}T10:30:00Z", "Furneux", "red", "purple", 1000),
-
-    Flight(COLEMORE, 27000, 5, 5, f"{DATE2}T10:30:00Z", "Colemore", "green", "purple", 600),
-    Flight(COLEMORE, 20000, 4, 5, f"{DATE2}T10:30:00Z", "Colemore", "purple", "purple", 350),
-    Flight(COLEMORE, 12700, 3, 5, f"{DATE2}T10:30:00Z", "Colemore", "orange", "purple", 200),
-    Flight(COLEMORE, 30000, 5, 5, f"{DATE2}T10:30:00Z", "Colemore", "red", "purple", 1000),
+    #Flight(COLEMORE, 27000, 5, 5, f"{DATE2}T10:30:00Z", "Colemore", "green", "purple", 600),
+    #Flight(COLEMORE, 20000, 4, 5, f"{DATE2}T10:30:00Z", "Colemore", "purple", "purple", 350),
+    #Flight(COLEMORE, 12700, 3, 5, f"{DATE2}T10:30:00Z", "Colemore", "orange", "purple", 200),
+    #Flight(COLEMORE, 30000, 5, 5, f"{DATE2}T10:30:00Z", "Colemore", "red", "purple", 1000),
 
     #Flight(VINCENT_SQUARE[0], VINCENT_SQUARE[1], 26295, 2, 5, f"{DATE2}T03:45:00Z", "Vincent Square", "blue", "red", 600),
     #Flight(VINCENT_SQUARE[0], VINCENT_SQUARE[1], 25698, 3, 5, f"{DATE2}T03:45:00Z", "Vincent Square", "purple", "red", 600),
@@ -99,6 +99,14 @@ class LocationMarker():
             dt_object = datetime.datetime.strptime(self.datetime, '%Y-%m-%dT%H:%M:%SZ') + datetime.timedelta(hours=1)
         return f'{dt_object.strftime("%a %d")}'
 
+    @property
+    def full_date(self):
+        if "." in self.datetime:
+            dt_object = datetime.datetime.strptime(self.datetime, '%Y-%m-%dT%H:%M:%S.%fZ') + datetime.timedelta(hours=1)
+        else:
+            dt_object = datetime.datetime.strptime(self.datetime, '%Y-%m-%dT%H:%M:%SZ') + datetime.timedelta(hours=1)
+        return f'{dt_object.strftime("%a %d %b")}'
+
 
 def get_flight_route_data(launch:Flight, flight_list):
     launch_longitude = float(launch.launch_longitude)
@@ -135,7 +143,7 @@ def get_all_flights() -> List[List[LocationMarker]]:
     return flights
 
 
-def draw_map(flights):
+def draw_launch_map(flights):
     m = folium.Map(location=[VINCENT_SQUARE[1], VINCENT_SQUARE[2]], zoom_start=9)
     m.get_root().html.add_child(folium.Element(f"""<h3 align="center" style="font-size:16px"><b>{TITLE}</b></h3>"""))
     for flight in flights:
@@ -151,7 +159,52 @@ def draw_map(flights):
 
     return m
 
-def generate_flights():
+
+def draw_hourly_map(flights):
+    m = folium.Map(location=[flights[0].launch_latitude, flights[0].launch_longitude], zoom_start=9)
+    m.get_root().html.add_child(folium.Element(f"""<h3 align="center" style="font-size:16px"><b>{HOURLY_TITLE} - {flights[0].launch_site}</b></h3>"""))
+    points = []
+    for flight in flights:
+        points.append((flight.markers[-1].latitude, flight.markers[-1].longitude))
+        folium.Marker((flight.markers[-1].latitude, flight.markers[-1].longitude),
+                      icon=folium.features.CustomIcon("static/img/target-1-sm.png", icon_size=(10, 10)),
+                      popup=f"<b>Landing Site</b><br>Burst_Height:{flight.burst_altitude}m<br>Time:{flight.markers[-1].time}<br>Date:{flight.markers[-1].full_date}<br>Balloon size : {flight.balloon_size}g<br>Ascent Rate : {flight.ascent_rate}m/s").add_to(m)
+    folium.PolyLine(points, color=flight.line_colour, weight=1.2, opacity=1, ).add_to(m)
+    folium.Marker((flight.markers[0].latitude, flight.markers[0].longitude),
+                  icon=folium.features.CustomIcon("static/img/target-8-sm.png", icon_size=(10, 10)),
+                  popup=f"Launch site").add_to(m)
+    return m
+
+
+def generate_hourly_flights(location):
+    print("Generating flights...")
+    flights = []
+    flight_threads = []
+    flight_list:List[Flight] = []
+    start = datetime.datetime.now()
+    DAYS = 7
+    HOUR_GAP = 2
+    for index in range(0, int((24/HOUR_GAP) * DAYS)):
+        new_datetime = start + datetime.timedelta(hours=index*HOUR_GAP)
+        if 18 > new_datetime.hour > 6:
+            flights.append(Flight(location, 26000, 5, 5, f"{new_datetime.strftime('%Y-%m-%dT%H:%M:%S')}Z", location[0], "pink", "red", 600))
+
+    for flight in flights:
+        flight_thread = threading.Thread(target=get_flight_route_data, args=(flight, flight_list))
+        flight_thread.start()
+        print("Thread started!")
+        flight_threads.append(flight_thread)
+    for flight_thread in flight_threads:
+        flight_thread.join()
+
+    flight_list.sort(key=lambda f: f.launch_datetime)
+    return draw_hourly_map(flight_list)
+
+
+    print()
+
+
+def generate_launch_flights():
     print("Generating flights...")
 
     flight_threads = []
@@ -168,4 +221,4 @@ def generate_flights():
         flight_thread.join()
 
     print("All threads done")
-    return draw_map(flight_list)
+    return draw_launch_map(flight_list)
